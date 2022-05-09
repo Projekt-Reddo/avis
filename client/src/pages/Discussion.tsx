3b4f07417bf0f5f9c8fdb5bd5675d19657d90fad
ViNguyenHoang <hoangvi0206@gmail.com>
2022-05-09 09:23:53 +0700

    Code Divocer Page on PC UI

--- client/src/pages/Discussion.tsx
+++ client/src/pages/Discussion.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { IonContent, IonPage } from "@ionic/react";
 
+import PostCard from "../components/PostCard";
+import HumCard from "../components/HumCard";
+import TrendingCard from "../components/TrendingCard";
+import SearchBox from "../components/SearchBox";
 const Discussion = () => {
-    return <div>Discussion</div>;
+    return (
+        <IonPage>
+            <IonContent>
+                <div className="grid grid-cols-2 md:grid-cols-3 md:gap-6 m-4 md:m-16">
+                    <div className="col-span-2">
+                        <PostCard />
+                        <HumCard />
+                        <HumCard />
+                        <HumCard />
+                    </div>
+                    <div className=" hidden md:col-span-1 md:block">
+                        <SearchBox />
+                        <TrendingCard />
+                        <TrendingCard />
+                    </div>
+                </div>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Discussion;
