60099d70675b9e0acfe3c261b70a492002834701
unknown <phucnnh21@gmail.com>
2022-05-14 15:48:39 +0700

    config front-end: absolute import & re-structure project: page components and shared components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import {
     IonApp,
-    IonIcon,
     IonLabel,
     IonRouterOutlet,
     IonTabBar,
@@ -9,7 +8,8 @@ import {
     setupIonicReact,
 } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
-import { ellipse, square, triangle } from "ionicons/icons";
+
+import "./theme/index.css";
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
@@ -30,8 +30,6 @@ import "@ionic/react/css/display.css";
 /* Theme variables */
 import "./theme/variables.css";
 
-import "./theme/index.css";
-
 import MainRoute from "./routes";
 
 setupIonicReact();
@@ -43,17 +41,15 @@ const App: React.FC = () => (
                 <IonRouterOutlet>
                     <MainRoute />
                 </IonRouterOutlet>
-                <IonTabBar slot="bottom">
+
+                <IonTabBar slot="top">
                     <IonTabButton tab="tab1" href="/tab1">
-                        <IonIcon icon={triangle} />
                         <IonLabel>Tab 1</IonLabel>
                     </IonTabButton>
                     <IonTabButton tab="tab2" href="/tab2">
-                        <IonIcon icon={ellipse} />
                         <IonLabel>Tab 2</IonLabel>
                     </IonTabButton>
                     <IonTabButton tab="tab3" href="/tab3">
-                        <IonIcon icon={square} />
                         <IonLabel>Tab 3</IonLabel>
                     </IonTabButton>
                 </IonTabBar>
