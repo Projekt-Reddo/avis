82626d3a6c9f6fdac02f428035e02e526474acb6
Thiện Phú Trần <hostcode0301@gmail.com>
2022-05-15 15:56:46 +0700

    add White mode color config

--- client/src/components/shared/Nav.tsx
+++ client/src/components/shared/Nav.tsx
@@ -6,22 +6,28 @@ import Icon from "./Icon";
 
 const Nav = () => {
     return (
-        <nav className="flex flex-row justify-around items-center p-2 nav-height z-50 bg-black">
+        <nav className="flex flex-row justify-around items-center p-2 nav-height z-50 bg-[color:var(--nav-bg-color)] border-b-[.5px] border-b-[color:var(--nav-border-color)]">
             <div>
                 <Link to="/">
-                    <span className="font-bold text-pink-500">LOGO</span>
+                    <span className="font-extrabold text-pink-500">LOGO</span>
                 </Link>
             </div>
 
-            <div className="flex flex-row justify-between items-center w-96">
-                <Link to="/search">Search</Link>
-                <Link to="/discussion">Discover</Link>
-                <Link to="/feedback">Feedback</Link>
+            <div className="flex flex-row justify-between items-center w-96 font-bold">
+                <Link className="hover:border-b" to="/search">
+                    Search
+                </Link>
+                <Link className="hover:border-b" to="/discussion">
+                    Discover
+                </Link>
+                <Link className="hover:border-b" to="/feedback">
+                    Feedback
+                </Link>
             </div>
 
-            <div className="">
+            <div className="font-bold">
                 <Link to="/login">
-                    <span className="font-medium">
+                    <span className="font-bold">
                         <Icon icon="right-to-bracket" /> Login/Register
                     </span>
                 </Link>
