ebfd5c23d694d6f9c162ddd8531775762e9732f0
phucnnh21 <phucnnh21@gmail.com>
2022-09-05 13:13:13 +0700

    add types and helpers for front-end app

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,7 +5,7 @@ import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
 import { Provider } from "react-redux";
-import store from "./store/store";
+import store from "./store";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
