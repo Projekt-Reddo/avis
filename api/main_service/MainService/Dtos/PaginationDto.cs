539bae26a50a8892f64807e799d94b77b505ac0b
Thiện Phú Trần <hostcode0301@gmail.com>
2022-09-08 09:14:00 +0700

    fix: add pagination dto

---
+++ api/main_service/MainService/Dtos/PaginationDto.cs
@@ -0,0 +1,17 @@
+namespace MainService.Dtos;
+
+public class PaginationReqDto<T>
+{
+    public int Page { get; set; }
+
+    public int Size { get; set; }
+
+    public T Filter { get; set; } = default(T)!;
+}
+
+public class PaginationResDto<T>
+{
+    public int Total { get; set; } = 0;
+
+    public IEnumerable<T> Payload { get; set; } = default(IEnumerable<T>)!;
+}
