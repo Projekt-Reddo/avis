431123787f3a566de9d7c07b181b633c5e42de75
hostcode0301 <hostcode0301@gmail.com>
2022-09-12 14:05:25 +0700

    try to fix IonPage & IonContent manually by using custome div

--- client/src/components/shared/PageWrapper.tsx
+++ client/src/components/shared/PageWrapper.tsx
@@ -1,5 +1,6 @@
 import { MOBILE_BREAKPOINT } from "utils/constants";
 import { useWindowDimensions } from "utils/useWindowDimensions";
+import "theme/PageWrapper.css";
 
 interface PageWrapperProps {
     children?: React.ReactNode;
@@ -16,7 +17,7 @@ const PageWrapper: React.FC<PageWrapperProps> = ({
 
     return (
         <div
-            className={`${
+            className={`page-wrapper px-0 lg:px-32 2xl:px-52 ${
                 width! >= MOBILE_BREAKPOINT ? "padding-nav" : " "
             } min-height-nav w-full ${className}`}
             style={{ ...style }}
