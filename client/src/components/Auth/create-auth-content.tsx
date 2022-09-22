2d627100325c6f3b4b9c37b307438a5bccd83ff8
phucnnh21 <phucnnh21@gmail.com>
2022-09-22 17:49:25 +0700

    added Login Form

--- client/src/components/Auth/create-auth-content.tsx
+++ client/src/components/Auth/create-auth-content.tsx
@@ -1,16 +1,19 @@
 import SignUpForm from "./SignUpForm";
 import AuthFooter from "./AuthFooter";
 import AuthHeader from "./AuthHeader";
+import LoginForm from "./LoginForm";
 
 const createAuthContent = (pageType: "signup" | "login" = "signup") => {
-    const footerData =
+    const pageRender =
         pageType === "signup"
             ? {
+                  form: <SignUpForm />,
                   message: "Already have an account?",
                   url: "/login",
                   title: "Login now",
               }
             : {
+                  form: <LoginForm />,
                   message: "You haven't have an account?",
                   url: "/signup",
                   title: "Sign up now",
@@ -19,10 +22,8 @@ const createAuthContent = (pageType: "signup" | "login" = "signup") => {
     return (
         <div className="w-full h-full flex flex-col items-center justify-between p-12 lg:p-16">
             <AuthHeader />
-            <div className="w-full">
-                <SignUpForm />
-            </div>
-            <AuthFooter {...footerData} />
+            <div className="w-full">{pageRender.form}</div>
+            <AuthFooter {...pageRender} />
         </div>
     );
 };
