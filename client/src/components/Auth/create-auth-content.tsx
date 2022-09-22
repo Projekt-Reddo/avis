0995fc0b0b34bb61e2ae4c096352df4f829faec6
phucnnh21 <phucnnh21@gmail.com>
2022-09-22 17:09:18 +0700

    added layout for login

--- client/src/components/Auth/create-auth-content.tsx
+++ client/src/components/Auth/create-auth-content.tsx
@@ -3,13 +3,26 @@ import AuthFooter from "./AuthFooter";
 import AuthHeader from "./AuthHeader";
 
 const createAuthContent = (pageType: "signup" | "login" = "signup") => {
+    const footerData =
+        pageType === "signup"
+            ? {
+                  message: "Already have an account?",
+                  url: "/login",
+                  title: "Login now",
+              }
+            : {
+                  message: "You haven't have an account?",
+                  url: "/signup",
+                  title: "Sign up now",
+              };
+
     return (
         <div className="w-full h-full flex flex-col items-center justify-between p-12 lg:p-16">
             <AuthHeader />
             <div className="w-full">
                 <SignUpForm />
             </div>
-            <AuthFooter pageType={pageType} />
+            <AuthFooter {...footerData} />
         </div>
     );
 };
