9044da9f735d0d92dece551d4dbc55c59e161459
hostcode0301 <hostcode0301@gmail.com>
2022-10-11 16:52:01 +0700

    feat: genre manage page; fix: create & delete logic when partial success

--- client/src/store/slices/genreSlice.ts
+++ client/src/store/slices/genreSlice.ts
@@ -1,8 +1,14 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { recommendGenreApi } from "api/genre-api";
+import {
+    recommendGenreApi,
+    viewGenreApi,
+    createGenreApi,
+    deleteGenreApi,
+} from "api/genre-api";
+import { addToast } from "./toastSlice";
 
 const initialState: AsyncReducerInitialState = {
-    status: "idle",
+    status: "init",
     data: null,
     error: null,
 };
@@ -24,6 +30,27 @@ const genreSlice = createSlice({
             .addCase(recommendAsync.fulfilled, (state, action) => {
                 state.status = "idle";
                 state.data = action.payload;
+            })
+            .addCase(viewAsync.pending, (state) => {
+                state.status = "loading";
+            })
+            .addCase(viewAsync.fulfilled, (state, action) => {
+                state.status = "idle";
+                state.data = action.payload;
+            })
+            .addCase(createAsync.pending, (state) => {
+                state.status = "loading";
+            })
+            .addCase(createAsync.fulfilled, (state, action) => {
+                state.status = "idle";
+                state.data = action.payload;
+            })
+            .addCase(deleteAsync.pending, (state) => {
+                state.status = "loading";
+            })
+            .addCase(deleteAsync.fulfilled, (state, action) => {
+                state.status = "idle";
+                state.data = action.payload;
             });
     },
 });
@@ -34,6 +61,62 @@ export const recommendAsync = createAsyncThunk(
         const res = await recommendGenreApi(keyword);
 
         return res;
+    }
+);
+
+export const viewAsync = createAsyncThunk("genre/view", async () => {
+    const res = await viewGenreApi();
+
+    return res;
+});
+
+export const createAsync = createAsyncThunk(
+    "genre/create",
+    async (names: string[], thunkApi) => {
+        try {
+            const res = await createGenreApi(names);
+
+            thunkApi.dispatch(
+                addToast({
+                    variant: "primary",
+                    message: res.message,
+                })
+            );
+            return await viewGenreApi();
+        } catch (e: any) {
+            thunkApi.dispatch(
+                addToast({
+                    variant: "warning",
+                    message: e.response.data.message,
+                })
+            );
+            return await viewGenreApi();
+        }
+    }
+);
+
+export const deleteAsync = createAsyncThunk(
+    "genre/delete",
+    async (ids: string[], thunkApi) => {
+        try {
+            const res = await deleteGenreApi(ids);
+
+            thunkApi.dispatch(
+                addToast({
+                    variant: "primary",
+                    message: res.message,
+                })
+            );
+            return await viewGenreApi();
+        } catch (e: any) {
+            thunkApi.dispatch(
+                addToast({
+                    variant: "warning",
+                    message: e.response.data.message,
+                })
+            );
+            return await viewGenreApi();
+        }
     }
 );
 
