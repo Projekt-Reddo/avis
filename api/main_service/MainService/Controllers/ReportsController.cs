0ef9c3e29a36bfed9645589f0fc2c79608b46505
marshmary <hostcode0301@gmail.com>
2022-10-27 16:07:33 +0700

    feat: list reports api

--- api/main_service/MainService/Controllers/ReportsController.cs
+++ api/main_service/MainService/Controllers/ReportsController.cs
@@ -11,36 +11,42 @@ namespace MainService.Controllers;
 [Route("api/[controller]")]
 public class ReportsController : ControllerBase
 {
-    private readonly IReportLogic _reportLogic;
+	private readonly IReportLogic _reportLogic;
 
-    public ReportsController(IReportLogic reportLogic)
-    {
-        _reportLogic = reportLogic;
-    }
+	public ReportsController(IReportLogic reportLogic)
+	{
+		_reportLogic = reportLogic;
+	}
 
-    [Authorize]
-    [HttpPost]
-    public async Task<ActionResult<ResponseDto>> ReportPost(ReportCreateDto reportDto)
-    {
-        ReportLogicResponse rs = new ReportLogicResponse();
-        var userId = User.FindFirst(JwtTokenPayload.USER_ID)!.Value; // Get user id from token
-        reportDto.UserId = userId;
+	[Authorize]
+	[HttpPost]
+	public async Task<ActionResult<ResponseDto>> ReportPost(ReportCreateDto reportDto)
+	{
+		ReportLogicResponse rs = new ReportLogicResponse();
+		var userId = User.FindFirst(JwtTokenPayload.USER_ID)!.Value; // Get user id from token
+		reportDto.UserId = userId;
 
-        if (String.IsNullOrWhiteSpace(reportDto.PostId) && String.IsNullOrWhiteSpace(reportDto.CommentId))
-        {
-            return BadRequest(new ResponseDto(400, ResponseMessage.REPORT_NO_IDS));
-        }
+		if (String.IsNullOrWhiteSpace(reportDto.PostId) && String.IsNullOrWhiteSpace(reportDto.CommentId))
+		{
+			return BadRequest(new ResponseDto(400, ResponseMessage.REPORT_NO_IDS));
+		}
 
-        if (!String.IsNullOrWhiteSpace(reportDto.PostId))
-        {
-            rs = await _reportLogic.PostReport(reportDto);
-        }
+		if (!String.IsNullOrWhiteSpace(reportDto.PostId))
+		{
+			rs = await _reportLogic.PostReport(reportDto);
+		}
 
-        if (!String.IsNullOrWhiteSpace(reportDto.CommentId))
-        {
-            rs = await _reportLogic.CommentReport(reportDto);
-        }
+		if (!String.IsNullOrWhiteSpace(reportDto.CommentId))
+		{
+			rs = await _reportLogic.CommentReport(reportDto);
+		}
 
-        return StatusCode(rs.StatusCode, new ResponseDto(rs.StatusCode, rs.Message));
-    }
+		return StatusCode(rs.StatusCode, new ResponseDto(rs.StatusCode, rs.Message));
+	}
+
+	[HttpPost("filter")]
+	public async Task<ActionResult<PaginationResDto<ReportReadDto>>> GetReports(PaginationReqDto<ReportFilterDto> pagination)
+	{
+		return Ok(await _reportLogic.List(pagination));
+	}
 }
