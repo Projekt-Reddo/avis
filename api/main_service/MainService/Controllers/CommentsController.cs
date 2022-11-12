76518afea5f09e8daeab638962c0d79d617eb933
marshmary <hostcode0301@gmail.com>
2022-11-12 15:56:55 +0700

    feat: comment delete

--- api/main_service/MainService/Controllers/CommentsController.cs
+++ api/main_service/MainService/Controllers/CommentsController.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using MainService.Data;
 using MainService.Dtos;
+using MainService.Filters;
 using MainService.Logic;
 using MainService.Models;
 using MainService.Utils;
@@ -12,128 +13,143 @@ using static Constants;
 
 namespace MainService.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class CommentsController : ControllerBase
-    {
-        private readonly IMapper _mapper;
-        private readonly IPostRepo _postRepo;
-        private readonly IAccountRepo _accountRepo;
-        private readonly ICommentRepo _commentRepo;
-        private readonly ICommentLogic _commentLogic;
-        private readonly IAccountLogic _accountLogic;
+	[ApiController]
+	[Route("api/[controller]")]
+	public class CommentsController : ControllerBase
+	{
+		private readonly IMapper _mapper;
+		private readonly IPostRepo _postRepo;
+		private readonly IAccountRepo _accountRepo;
+		private readonly ICommentRepo _commentRepo;
+		private readonly ICommentLogic _commentLogic;
+		private readonly IAccountLogic _accountLogic;
 
-        public CommentsController(IMapper mapper, IAccountRepo accountRepo, IPostRepo postRepo, ICommentRepo commentRepo,
-            ICommentLogic commentLogic, IAccountLogic accountLogic)
-        {
-            _mapper = mapper;
-            _commentRepo = commentRepo;
-            _commentLogic = commentLogic;
-            _postRepo = postRepo;
-            _accountRepo = accountRepo;
-            _accountLogic = accountLogic;
-        }
+		public CommentsController(IMapper mapper, IAccountRepo accountRepo, IPostRepo postRepo, ICommentRepo commentRepo,
+			ICommentLogic commentLogic, IAccountLogic accountLogic)
+		{
+			_mapper = mapper;
+			_commentRepo = commentRepo;
+			_commentLogic = commentLogic;
+			_postRepo = postRepo;
+			_accountRepo = accountRepo;
+			_accountLogic = accountLogic;
+		}
 
-        [Authorize]
-        [HttpPost]
-        public async Task<ActionResult<ResponseDto>> AddComment([FromForm] CommentCreateDto newComment)
-        {
-            var comment = _mapper.Map<Comment>(newComment);
+		[Authorize]
+		[HttpPost]
+		public async Task<ActionResult<ResponseDto>> AddComment([FromForm] CommentCreateDto newComment)
+		{
+			var comment = _mapper.Map<Comment>(newComment);
 
-            // Add comment into the repo first
-            var rs = await _commentRepo.AddOneAsync(comment);
+			// Add comment into the repo first
+			var rs = await _commentRepo.AddOneAsync(comment);
 
-            if (newComment.PostId != null)
-            {// if that comment is the 1st level comment
-                // Update Post comment ids
-                var rss = await _commentLogic.UpdatePostComment(newComment.PostId, rs);
-            }
+			if (newComment.PostId != null)
+			{// if that comment is the 1st level comment
+			 // Update Post comment ids
+				var rss = await _commentLogic.UpdatePostComment(newComment.PostId, rs);
+			}
 
-            if (newComment.CommentId != null)
-            {
-                // Update comment's child comment ids
-                var commentFromRepo = await _commentLogic.GetCommentById(newComment.CommentId);
+			if (newComment.CommentId != null)
+			{
+				// Update comment's child comment ids
+				var commentFromRepo = await _commentLogic.GetCommentById(newComment.CommentId);
 
-                if (commentFromRepo.Comments == null)
-                {
-                    commentFromRepo.Comments = new List<ObjectId>();
-                    // commentFromRepo.Comments = new List<string>();
-                }
-                commentFromRepo.Comments.Add(new ObjectId(comment.Id));
-                // commentFromRepo.Comments.Add(comment.Id);
+				if (commentFromRepo.Comments == null)
+				{
+					commentFromRepo.Comments = new List<ObjectId>();
+					// commentFromRepo.Comments = new List<string>();
+				}
+				commentFromRepo.Comments.Add(new ObjectId(comment.Id));
+				// commentFromRepo.Comments.Add(comment.Id);
 
-                var rss = await _commentLogic.UpdateComment(newComment.CommentId, commentFromRepo);
-            }
+				var rss = await _commentLogic.UpdateComment(newComment.CommentId, commentFromRepo);
+			}
 
-            var isMediaExist = newComment.Media != null;
-            if (isMediaExist)
-            {
+			var isMediaExist = newComment.Media != null;
+			if (isMediaExist)
+			{
 
-                (var isMp3File, var songCheckMessage) = FileExtension.CheckMp3Extension(newComment.Media!);
-                (var isImageFile, var imageCheckMessage) = FileExtension.CheckImageExtension(newComment.Media!);
-                (var isVideoFile, var videoCheckMessage) = FileExtension.CheckVideoExtension(newComment.Media!);
+				(var isMp3File, var songCheckMessage) = FileExtension.CheckMp3Extension(newComment.Media!);
+				(var isImageFile, var imageCheckMessage) = FileExtension.CheckImageExtension(newComment.Media!);
+				(var isVideoFile, var videoCheckMessage) = FileExtension.CheckVideoExtension(newComment.Media!);
 
-                if (!isMp3File && !isImageFile && !isVideoFile)
-                {
-                    return BadRequest(new ResponseDto(400, ResponseMessage.COMMENT_CREATE_MEDIA_FAIL));
-                }
-                var mediaType = "";
-                var folderUpload = "";
-                if (isMp3File)
-                {
-                    folderUpload = S3Config.AUDIOS_FOLDER;
-                    mediaType = "audio";
-                }
-                else if (isImageFile)
-                {
-                    folderUpload = S3Config.IMAGES_FOLDER;
-                    mediaType = "image";
-                }
-                else
-                {// is video
-                    folderUpload = S3Config.VIDEOS_FOLDER;
-                    mediaType = "video";
-                }
-                comment.Media = new Media()
-                {
-                    Id = ObjectId.GenerateNewId().ToString(),
-                };
-                var commentUploadStatus = await _commentLogic.UploadNewComment(
-                comment,
-                newComment.Media!.OpenReadStream(),
-                newComment.Media.ContentType,
-                FileExtension.GetFileExtension(newComment.Media),
-                folderUpload,
-                mediaType
-                );
-            }
-            return Ok(new ResponseDto(200, ResponseMessage.COMMENT_CREATE_SUCCESS));
-        }
+				if (!isMp3File && !isImageFile && !isVideoFile)
+				{
+					return BadRequest(new ResponseDto(400, ResponseMessage.COMMENT_CREATE_MEDIA_FAIL));
+				}
+				var mediaType = "";
+				var folderUpload = "";
+				if (isMp3File)
+				{
+					folderUpload = S3Config.AUDIOS_FOLDER;
+					mediaType = "audio";
+				}
+				else if (isImageFile)
+				{
+					folderUpload = S3Config.IMAGES_FOLDER;
+					mediaType = "image";
+				}
+				else
+				{// is video
+					folderUpload = S3Config.VIDEOS_FOLDER;
+					mediaType = "video";
+				}
+				comment.Media = new Media()
+				{
+					Id = ObjectId.GenerateNewId().ToString(),
+				};
+				var commentUploadStatus = await _commentLogic.UploadNewComment(
+				comment,
+				newComment.Media!.OpenReadStream(),
+				newComment.Media.ContentType,
+				FileExtension.GetFileExtension(newComment.Media),
+				folderUpload,
+				mediaType
+				);
+			}
+			return Ok(new ResponseDto(200, ResponseMessage.COMMENT_CREATE_SUCCESS));
+		}
 
-        // [Authorize]
-        [HttpPost("filter")]
-        public async Task<ActionResult<PaginationResDto<CommentReadDto>>> GetComments(PaginationReqDto<CommentFilterDto> pagination)
-        {
-            // Pagination formula
-            var skipPage = (pagination.Page - 1) * pagination.Size;
+		// [Authorize]
+		[HttpPost("filter")]
+		public async Task<ActionResult<PaginationResDto<CommentReadDto>>> GetComments(PaginationReqDto<CommentFilterDto> pagination)
+		{
+			// Pagination formula
+			var skipPage = (pagination.Page - 1) * pagination.Size;
 
-            (var total, var comments) = await _commentLogic.GetComments(pagination.Filter.ObjectId!, pagination.Filter.IsPostChild, pagination.Size, skipPage);
+			(var total, var comments) = await _commentLogic.GetComments(pagination.Filter.ObjectId!, pagination.Filter.IsPostChild, pagination.Size, skipPage);
 
-            var commentDtos = _mapper.Map<IEnumerable<CommentReadDto>>(comments);
+			var commentDtos = _mapper.Map<IEnumerable<CommentReadDto>>(comments);
 
-            return Ok(new PaginationResDto<CommentReadDto>((int)total, commentDtos));
-        }
+			return Ok(new PaginationResDto<CommentReadDto>((int)total, commentDtos));
+		}
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<CommentReadDto>> GetCommentById(string id)
-        {
-            var comment = await _commentLogic.GetCommentById(id);
+		[HttpGet("{id}")]
+		public async Task<ActionResult<CommentReadDto>> GetCommentById(string id)
+		{
+			var comment = await _commentLogic.GetCommentById(id);
 
-            if (comment is null)
-            {
-                return BadRequest(new ResponseDto(404));
-            }
-            return _mapper.Map<CommentReadDto>(comment);
-        }
-    }
+			if (comment is null)
+			{
+				return BadRequest(new ResponseDto(404));
+			}
+			return _mapper.Map<CommentReadDto>(comment);
+		}
+
+		[Authorize]
+		[HttpDelete("{id}")]
+		[AuthorizeResource(ResourceType = ResourceType.Comment, IdField = "id")]
+		public async Task<ActionResult<ResponseDto>> DeleteComment(string id)
+		{
+			var rs = await _commentLogic.DeleteComment(id);
+
+			if (rs is false)
+			{
+				return BadRequest(new ResponseDto(400));
+			}
+
+			return Ok(new ResponseDto(200));
+		}
+	}
 }
--- client/src/pages/Comment.tsx
+++ client/src/pages/Comment.tsx
@@ -8,6 +8,7 @@ import React, { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { commentDetailAsync } from "store/slices/commentDetailSlice";
 import { useAppDispatch, useAppSelector } from "utils/react-redux-hooks";
+import NotFound from "components/shared/NotFound";
 
 interface CommentParams {
     commentId: string;
@@ -39,20 +40,33 @@ const Comment = () => {
             <div className="lg:grid lg:grid-cols-3 lg:gap-6 lg:mt-4">
                 {/* Left */}
                 <div className="w-full lg:col-span-2">
-                    {commentDetailState?.status !== "idle" ? (
-                        ""
-                    ) : (
-                        <div className="hum-card min-w-[20rem] p-4 lg:mb-4 border-t-0">
-                            <CommentCard comment={commentDetailState?.data} />
+                    {commentDetailState.status === "loading" ||
+                    commentDetailState.status === "init" ? (
+                        <div className="w-full grid place-items-center">
+                            <Loading />
                         </div>
-                    )}
+                    ) : commentDetailState.status === "error" ||
+                      (commentDetailState.status === "idle" &&
+                          !commentDetailState.data) ? (
+                        <div className="w-full grid place-items-center">
+                            <NotFound />
+                        </div>
+                    ) : (
+                        <>
+                            <div className="hum-card min-w-[20rem] p-4 lg:mb-4 border-t-0">
+                                <CommentCard
+                                    comment={commentDetailState?.data}
+                                />
+                            </div>
 
-                    {/* It's child comments */}
-                    <CommentSection
-                        key={commentId}
-                        postId={commentId}
-                        isPostChild={false}
-                    />
+                            {/* It's child comments */}
+                            <CommentSection
+                                key={commentId}
+                                postId={commentId}
+                                isPostChild={false}
+                            />
+                        </>
+                    )}
                 </div>
 
                 {/* Right */}
